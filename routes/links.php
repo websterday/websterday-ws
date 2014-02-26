a9f3edb04a81b395fa4ac1ad97df37a2d0e1ad46
Aurélien Praga <aurelien.praga@gmail.com>
2014-02-26 14:04:12 +0100

    AddLink : secure select + add user_id and status

--- routes/links.php
+++ routes/links.php
@@ -98,6 +98,7 @@ function search($value) {
 
 		$userId = getUser($app->request()->get('token'), $db);
 
+		// get the links with url corresponding to the search
 		$sql = 'SELECT id, url FROM links WHERE url LIKE :url';
 
 		$stmt = $db->prepare($sql);
@@ -123,13 +124,23 @@ function addLink() {
 		$db = getConnection();
 
 		$userId = getUser($app->request()->get('token'), $db);
+
 		$url = $app->request()->post('url');
-		$parsed_url = parse_url($url);
+
+		$parsedUrl = parse_url($url);
+
 		$folderId = ($app->request()->post('folder_id') ? $app->request()->post('folder_id') : null);
 		
-		// Ckecking if link already exist in database
-		$sql = 'SELECT COUNT(*) AS exist FROM links WHERE url = "'.$url.'";';
-		$exist = $db->query($sql)->fetchColumn();
+		// Checking if the link already exist in database
+		$sql = 'SELECT * FROM links WHERE url = :url AND user_id = :userId AND status = 1';
+		$stmt = $db->prepare($sql);
+
+		$stmt->bindParam(':url', $url);
+		$stmt->bindParam(':userId', $userId);
+		
+		$stmt->execute();
+
+		$exist = $stmt->fetchColumn();
 		
 		if ($exist) {	   // link already exist
 			$sql = 'UPDATE links SET count = count + 1, updated = NOW() WHERE url = :url;';
@@ -141,7 +152,7 @@ function addLink() {
 			$stmt = $db->prepare($sql);
 
 			$stmt->bindParam(':url', $url);
-			$stmt->bindParam(':domain', $parsed_url['host']);
+			$stmt->bindParam(':domain', $parsedUrl['host']);
 			$stmt->bindParam(':userId', $userId);
 			$stmt->bindParam(':folderId', $folderId);
 		}
